97b6cb9bb94e51bfd76a1c364556b1da451d6a05
paulhybryant <paulhybryant@gmail.com>
2020-10-29 15:04:18 +0800

    .

--- v2ray/ssl_update.sh
+++ v2ray/ssl_update.sh
@@ -2,12 +2,22 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-v2ray_qr_config_file="/usr/local/vmess_qr.json"
+# v2ray_qr_config_file="/usr/local/vmess_qr.json"
 # domain=$(grep '\"add\"' $v2ray_qr_config_file | awk -F '"' '{print $4}')
-
 
 systemctl stop nginx &> /dev/null
 sleep 1
+
+# "/root/.acme.sh/"acme.sh --issue \
+  # -d xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d www.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d aria2.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d baidupcs.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d calibre.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d nextcloud.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d zsxq.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # --standalone -k ec-256 --force
+
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
 # "/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc
 "/root/.acme.sh"/acme.sh --installcert \
