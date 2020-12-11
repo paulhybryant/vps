8a5f0e66026e9fdf46845144fdcb2322634d9e9d
paulhybryant <paulhybryant@gmail.com>
2020-12-11 08:54:17 +0800

    .

--- v2ray/ssl_update.sh
+++ v2ray/ssl_update.sh
@@ -9,25 +9,25 @@ systemctl stop nginx &> /dev/null
 sleep 1
 
 # "/root/.acme.sh/"acme.sh --issue \
-  # -d xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d www.xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d aria2.xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d baidupcs.xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d calibre.xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d nextcloud.xn--4gqp3krlw6xiiha30z1ui.tk \
-  # -d zsxq.xn--4gqp3krlw6xiiha30z1ui.tk \
+  # -d xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d aria2.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d baidupcs.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d calibre.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d nextcloud.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d www.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  # -d zsxq.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
   # --standalone -k ec-256 --force
 
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
 # "/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc
 "/root/.acme.sh"/acme.sh --installcert \
-  -d xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d www.xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d aria2.xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d baidupcs.xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d calibre.xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d nextcloud.xn--4gqp3krlw6xiiha30z1ui.tk \
-  -d zsxq.xn--4gqp3krlw6xiiha30z1ui.tk \
+  -d xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d aria2.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d baidupcs.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d calibre.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d nextcloud.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d www.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
+  -d zsxq.xn--4gqp3kj4hgvhbnaj0m7o1g.cn \
   --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc
 sleep 1
 systemctl start nginx &> /dev/null
