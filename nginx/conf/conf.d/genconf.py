a4268f7ff34f9b16822b45d61dfe4af73b8ecc37
paulhybryant <paulhybryant@gmail.com>
2021-01-15 09:21:02 +0800

    .

---
+++ nginx/conf/conf.d/genconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import jinja2
+import json
+import os
+
+template_file = "conf.template"
+json_parameter_file = "conf.json"
+
+# read the contents from the JSON files
+print("Read JSON parameter file...")
+apps = json.load(open(json_parameter_file))
+
+# next we need to create the central Jinja2 environment and we will load
+# the Jinja2 template file (the two parameters ensure a clean output in the
+# configuration file)
+print("Create Jinja2 environment...")
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="."),
+                         trim_blocks=True,
+                         lstrip_blocks=True)
+template = env.get_template(template_file)
+
+# now create the templates
+print("Create templates...")
+for app in apps:
+    result = template.render(app)
+    f = open("%s.conf" % app["app"], "w")
+    f.write(result)
+    f.close()
+    print("Configuration '%s' created..." % (app['app'] + ".conf"))
+print("DONE")
